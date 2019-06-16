db95676802a7421f387528c7ff7b9cbaf6515255
yuvalnir <50593115+yuvalnir@users.noreply.github.com>
2019-06-16 17:49:43 +0300

    finished making a case for each function that we have :)

--- java/Project Directory/Project/controller/MyController.java
+++ java/Project Directory/Project/controller/MyController.java
@@ -9,62 +9,128 @@ public class MyController implements Controller{
 	public void update(Observable o, Object arg) {
 		
 		switch ((String)((Event)arg).getArr().get(0)) {
+		//---------Login Page------------
+		case "user_login":
+			/* 0 - Event name
+			   1 - User name
+			   2 - Password */
+			Models.CheckPasswordAndEmail((String)((Event)arg).getArr().get(1),(String)((Event)arg).getArr().get(2));
+			break;
 		
+		case "user_login_response":
+			/* 0 - Event name
+			   1 - Array list */
+			//rotem's function
+			break;
+			
+		case "user_register":
+			//dolev's function
+			break;
+			
+		case "user_register_response":
+			//rotem's function
+			break;
+		//-------------------------------
+		//---------User Settings---------
+		case "user_change_settings":
+			//dolev's function
+			break;
+			
+		case "user_change_settings_response":
+			//rotem's function
+			break;
+			
+		case "user_update":
+			//dolev's function
+			break;
+			
+		case "user_update_response":
+			//rotem's function
+			break;
+			
+		case "user_delete":
+			//dolev's function
+			break;
+			
+		case "user_delete_response":
+			//rotem's function
+			break;
+		//-------------------------------
+		//---------Menu Page-------------	
 		case "top_10":
 			Models.top10();
 			break;
 			
 		case "top_10_response":
 			//rotem's function
+			break;	
+			
+		case "menu_search":
+			//dolev's function
 			break;
 			
-		case "user_login":
-			/*
-			 * 0 - Event name
-			 * 1 - User name
-			 * 2 - Password
-			 */
-			Models.CheckPasswordAndEmail((String)((Event)arg).getArr().get(1),(String)((Event)arg).getArr().get(2));
-			break;
-			
-		case "user_login_response":
-			/*
-			 * 0 - Event name
-			 * 1 - Array list
-			 */
+		case "menu_search_response":
 			//rotem's function
 			break;
 			
+		case "advenced_search":
+			//dolev's function
+			break;
+			
+		case "advenced_search_response":
+			//rotem's function
+			break;
+		//-------------------------------
+		//---------Recipe Page-----------
 		case "recipe_insert":
-		
-			//recipe.insert(); model function call
+			//dolev's function
+			break;
+			
+		case "recipe_insert_response":
+			//rotem's function
 			break;
 			
 		case "recipe_update":
+			//dolev's function
+			break;
 			
-			//recipe.update(); model function call
+		case "recipe_update_response":
+			//rotem's function
 			break;
 			
 		case "recipe_delete":
-		
-			//recipe.delete(); model function call
+			//dolev's function
 			break;
 			
+		case "recipe_delete_response":
+			//rotem's function
+			break;
+		//-------------------------------
+		//---------Ingredient Page-------
 		case "ingredient_insert":
-		
-			//ingredient.insert(); model function call
+			//dolev's function
+			break;
+			
+		case "ingredient_insert_response":
+			//rotem's function
 			break;
 			
 		case "ingredient_update":
-		
-			//ingredient.update(); model function call
+			//dolev's function
+			break;
+			
+		case "ingredient_update_response":
+			//rotem's function
 			break;
 			
 		case "ingredient_delete":
-			
-			//ingredient.delete(); model function call
+			//dolev's function
 			break;
-		}
-		
+			
+		case "ingredient_delete_response":
+			//rotem's function
+			break;
+		//-------------------------------
+		}	
 	}	
 }
--- java/Project Directory/Project/driver/MVCDriver.java
+++ java/Project Directory/Project/driver/MVCDriver.java
@@ -7,21 +7,13 @@ import model.Models;
 import view.View;
 import view.myView;
 
-
-
 public class MVCDriver {
 	public static void main(String[] args) {
 		Controller controller = new MyController();
 		model model = new Models();
 		View view = new myView();
 		
-		//create an object from every class
-		
 		model.addObserver(controller);
 		view.addObserver(controller);
-		
-		//add all the rest of the view observers
-		
-		//view.start();
 	}
 }
