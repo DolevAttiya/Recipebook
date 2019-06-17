cbb30d612be9d78e1880e537e16c3e5f58063309
rotemhy <50592753+rotemhy@users.noreply.github.com>
2019-06-17 13:30:44 +0300

    login2

--- java/Project Directory/Project/view/myView.java
+++ java/Project Directory/Project/view/myView.java
@@ -1,18 +1,32 @@
 package view;
 
 import java.awt.im.InputContext;
+import java.util.ArrayList;
 import java.util.Observable;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 import controller.Event;
 
 public class myView extends Observable implements View {
-
-	int login (String email, String pass)
+ User myUser;
+	public void login (String email, String pass)
 	{
 		Event ev=new Event();
-		if ((email=="rotem") || (pass=="1234"))
-			return 1;
-		else return 0;	
+		ev.getArr.add("userLogin");
+		ev.getArr.add(email);
+		ev.getArr.add(pass);
 		setChanged();
 		notifyObservers(ev);
+	}
+	
+	public void loginResponse (ArrayList<Object> us) {
+			if((ArrayList<User>)(us.get(1)).size()>0) // if the user exists in the DB
+			{
+				myUser=(ArrayList<User>)(us.get(1)).get(0));
+				MainPage frame = new MainPage();
+			}
+			else JOptionPane.showMessageDialog(null,"One of the parameters is wrong, Please try again");
 	}
 }
