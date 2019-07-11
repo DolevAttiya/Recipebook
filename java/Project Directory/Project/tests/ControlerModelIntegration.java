b6f4e845a36e29402092402139b4d3dd0d39a260
yuvalnir <50593115+yuvalnir@users.noreply.github.com>
2019-07-11 18:18:40 +0300

    tests are on their way!!!! and some other changes too.

--- java/Project Directory/Project/tests/ControlerModelIntegration.java
+++ java/Project Directory/Project/tests/ControlerModelIntegration.java
@@ -2,34 +2,46 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-//import controller.Controller;
 import controller.Event;
 import controller.MyController;
+import model.Models;
+import model.model;
 import view.View;
+import view.myView;
+import controller.Controller;
 
-import java.util.Observable;
-
-public class ControlerModelIntegration extends Observable implements View {
+public class ControlerModelIntegration {
 
 	Event ev;
-	/*Controller controller = new MyController();
-	ControlerModelIntegration test=new ControlerModelIntegration();
-	test.addObserver(controller);*/
-	ControlerModelIntegration tester;
-	
+	static model m;
+	static View v;
+	static Controller controllerTest;
+
+	@BeforeAll
+	static void setup() {
+		m = new Models();
+		v = new myView();
+		controllerTest = new MyController(m,v);
+		((Models)m).addObserver(controllerTest);
+	}
+
 	@Test
-	void userLoginTest()  {
+	void userLoginTest() {
 		ev = new Event();
-		int num=0;
 		ev.getArr().add("user_login");
 		ev.getArr().add("username");
 		ev.getArr().add("password");
-		setChanged();
-		notifyObservers(ev);
-		num=tester.countObservers();
-		assertTrue(MyController.visit);
+		((MyController)controllerTest).update(null, ev);
 	}
-
+	
+	@Test
+	void userRegisterTest() {
+		ev = new Event();
+		ev.getArr().add("user_register");
+		ev.getArr().add("username");
+		ev.getArr().add("password");
+		((MyController)controllerTest).update(null, ev);
+	}
 }
